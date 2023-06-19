9c7521d142afbe568c0a34008d0413325145e9d5
Gayathri Krishnan <gayathri.krishnan.designer@gmail.com>
2023-06-19 19:10:42 +0530

    Migrating the app from Goerli to Sepolia

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x89fc79D6fB64E563E5b840d972C2a62C19F11373";
+const WHITELIST_CONTRACT_ADDRESS = "0xeB07753bdD351A0F8E0d39C9c9b3f864965507Ed";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-green.vercel.app/api/";
 
