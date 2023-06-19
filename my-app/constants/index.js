24584525f005efbbbe72c7e795ff4b22e728fafa
Gayathri Krishnan <gayathri.krishnan.designer@gmail.com>
2023-06-19 19:23:13 +0530

    Updating the contract address

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -581,4 +581,4 @@ export const abi =  [
     "type": "receive"
   }
 ]
-export const NFT_CONTRACT_ADDRESS = "0x7046eE09a255C377D595933acd3B01f1Bc7FB65F"
+export const NFT_CONTRACT_ADDRESS = "0x2F586c5417482c8E44f311221B8fe0881b699027"
